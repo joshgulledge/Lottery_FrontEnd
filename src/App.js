3ba9fa9b6e4ece92fd8c561819ea40ead2a27c4e
joshgulledge <joshcalebgulledge@gmail.com>
2021-12-28 23:00:40 -0600

    set up a form to enter the lottery, send the enter to contract

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import web3Inst from './web3';
 import lottery from './lottery';
 
 function App() {
-
+  // local state variables
   const [manager, setManager] = useState("");
   const [players, setPlayers] = useState([]);
-  const [contractBalance, setContractBalance] = useState("");
+  const [contractBalance, setContractBalance] = useState(""); // will be in wei
+  const [amountEntered, setAmountEntered] = useState(""); // will be in eth
 
   useEffect(() => {
     // create a call to get the data from the contract
@@ -15,9 +16,11 @@ function App() {
       // this should actually go to the contract and retrieve the data
       let managerAddress = await lottery.methods.manager().call();
       let players = await lottery.methods.getPlayersList().call();
+      // balance is in wei and not ether
       let balance = await web3Inst.eth.getBalance(lottery.options.address);
+
       // set the state values with data from the contract
-      setContractBalance(balance);
+      setContractBalance(web3Inst.utils.fromWei(balance), 'ether'); // converts to eth
       setManager(managerAddress);
       setPlayers(players);
     };
@@ -31,23 +34,61 @@ function App() {
   console.log(players);
   console.log(contractBalance);
 
+  // local functions
+  async function enterLottery (e) {
+    e.preventDefault();
+    console.log(`Entering the lottery with ${amountEntered} eth`);
+
+    // send the transaction to the contract
+    const accounts = await web3Inst.eth.getAccounts();
+    await lottery.methods.enter().send({
+      // assume the first account is the main one we will be using
+      from: accounts[0],
+      // get the value entered and convert to wei from an ether amount
+      value: web3Inst.utils.toWei(amountEntered, 'ether')
+    });
+
+    
+  };
+
   return (
     <div>
       <h2>
         Lottery Contract 
       </h2>
 
-      <p>
-        Contract Managed by: {manager}
-      </p>
+      <div>
+        Contract Managed by: {manager} 
+      </div>
 
-      <p>
+      <div>
         Current number of players: {players.length}
-      </p>
+      </div>
 
-      <p>
-        Current balance of Lottery: {contractBalance}
-      </p>
+      <div>
+        Current balance of Lottery: {contractBalance} Eth
+      </div>
+
+      <form onSubmit={enterLottery}>
+        <h4>
+          Enter the Lottery today!
+        </h4>
+
+        <div>
+          <label>
+            Amount of Eth to Enter:
+            <input onChange={e => {
+              setAmountEntered(e.target.value);
+            }} type="number" />
+          </label>
+        </div>
+
+        <button>
+          Enter Lottery
+        </button>
+      </form>
+
+
     </div>
   );
 }
